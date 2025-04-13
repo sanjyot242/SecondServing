4f0eca563a76020e21176c8510d3bcfa9656d1f7
sanjyot242 <sanjyot.satvi242@gmail.com>
2025-04-12 22:38:10 -0700

    Integration

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from routes import app_router
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow requests from your frontend
+    allow_origins=["http://localhost:5173"],  # Allow requests from your frontend
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
