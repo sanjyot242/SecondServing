2ce4166bafdfad8768b6521b2dcdbd5120af57b4
iabhi4 <iamonecool@gmail.com>
2025-04-12 20:00:30 -0700

    Auth setup

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,21 @@
 #For type verification using pydantic
+
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class UserCreate(BaseModel):
+    name: str
+    location: str
+    type: str
+    contactInfo: str
+    password: str
+    email: str
+    role: str
+
+class Token(BaseModel):
+    user_id: int
+
+
+class TokenData(BaseModel):
+    email: str
